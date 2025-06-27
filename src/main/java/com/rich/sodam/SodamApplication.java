feaa0331da788d7122b1bb0961c4d62a0c3f5e5f
Eehoyoung <c65621@naver.com>
2025-06-27 19:04:16 +0900

    **feat: JPA 리포지토리 활성화 및 캐싱 설정 추가**
    - `@EnableJpaRepositories` 추가로 JPA 리포지토리 활성화
    - Hibernate 및 EhCache를 사용한 캐싱 관련 의존성 추가
    - Actuator의 일부 Health 및 Metrics 자동 구성 비활성화
    - `build.gradle`에 JCache, EhCache, Hibernate 의존성 추가
    
    Signed-off-by: Eehoyoung <c65621@naver.com>

--- src/main/java/com/rich/sodam/SodamApplication.java
+++ src/main/java/com/rich/sodam/SodamApplication.java
@@ -1,13 +1,19 @@
 package com.rich.sodam;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        MetricsAutoConfiguration.class,
+        HealthEndpointAutoConfiguration.class
+})
+@EnableJpaRepositories
 public class SodamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SodamApplication.class, args);
     }
-
 }
